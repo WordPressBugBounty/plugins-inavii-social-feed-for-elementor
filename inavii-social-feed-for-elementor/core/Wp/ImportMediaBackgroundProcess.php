3b8b5fe7804dbf0e775e7a6f6319b5c71603f084
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 05:11:14 +0000

    2.6.9

---
+++ inavii-social-feed-for-elementor/core/Wp/ImportMediaBackgroundProcess.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Inavii\Instagram\Wp;
+
+use Inavii\Instagram\RestApi\EndPoints\Media\GenerateThumbnailsProcessor;
+use Inavii\Instagram\RestApi\EndPoints\Media\ImporterMediaProcessor;
+
+class ImportMediaBackgroundProcess
+{
+    public function __construct()
+    {
+        add_action('inavii_instagram_import_hook', [$this, 'fetchInstagramMedia']);
+    }
+
+    public function registerEvents()
+    {
+        if (!wp_next_scheduled('inavii_instagram_import_hook')) {
+            wp_schedule_single_event(time(), 'inavii_instagram_import_hook');
+        }
+    }
+
+    public function scheduleMediaDownload($media)
+    {
+        $data = array_map(function($item) {
+            return [
+                'id' => $item['id'] ?? null,
+                'url' => $item['url'] ?? null,
+                'children' => $item['children'] ?? []
+            ];
+        }, $media ?? []);
+
+        $previousMedia = get_option('inavii_instagram_media_to_download', []);
+
+        update_option('inavii_instagram_media_to_download', array_merge($previousMedia, $data));
+    }
+
+    public function fetchInstagramMedia()
+    {
+        $media = get_option('inavii_instagram_media_to_download', []);
+
+        (new ImporterMediaProcessor())->task($media);
+        (new GenerateThumbnailsProcessor())->task($media);
+
+        delete_option('inavii_instagram_media_to_download');
+
+        wp_clear_scheduled_hook('inavii_instagram_import_hook');
+    }
+}
